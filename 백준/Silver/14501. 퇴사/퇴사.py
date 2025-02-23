5e05e893a9173c0b0f695907d2b841defc41ab6d
smallwanderer <99022143+smallwanderer@users.noreply.github.com>
2025-02-23 21:18:55 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,24 +1,17 @@
-import copy
-N = int(input())
+n = int(input())
+a = [0] * (n + 1)
+p = [0] * (n + 1)
+dp = [0] * (n + 2)
 
-def binary_tree():
-  result = {0: 0}
+for i in range(1, n + 1):
+    t, pay = map(int, input().split())
+    a[i] = t
+    p[i] = pay
 
-  for i in range(N):
-    T, P = map(int, input().split())
-    T += i
+for i in range(1, n + 1):
+    if i + a[i] <= n + 1:
+        dp[i + a[i]] = max(dp[i + a[i]], dp[i] + p[i])
 
-    if T > N:
-      continue
+    dp[i + 1] = max(dp[i + 1], dp[i])
 
-    temp = copy.deepcopy(result)
-    for key, val in temp.items():
-      if key <= i:
-        if T in result.keys():
-          result[T] = max(result[T], val+P)
-        else:
-          result[T] = val+P
-
-  print(result[max(result, key=result.get)])
-  
-binary_tree()
+print(max(dp))
