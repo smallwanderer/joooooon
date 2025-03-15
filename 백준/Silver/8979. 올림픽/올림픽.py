8a0385c75dde30f0efd5948bbd26efaabc7625e6
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-03-15 13:12:28 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -6,6 +6,9 @@ country.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
 
 rank = 1
 for c_idx in range(1,n):
+  if country[0][0] == k:
+    sys.stdout.write(f"{rank}")
+    break
   if country[c_idx][1:] != country[c_idx-1][1:]:
     rank += 1
   if country[c_idx][0] == k:
