c87c7af457db33bff708686958d55eff98777c5d
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-03-15 13:28:40 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 33564 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,15 +2,16 @@ import sys
 
 n, k = map(int, sys.stdin.readline().strip().split())
 country = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
-country.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
+country.sort(key=lambda x: (-x[1], -x[2], -x[3]))
 
-rank = 1
-for c_idx in range(1,n):
-  if country[0][0] == k:
-    sys.stdout.write(f"{rank}")
-    break
-  if country[c_idx][1:] != country[c_idx-1][1:]:
-    rank += 1
-  if country[c_idx][0] == k:
-    sys.stdout.write(f"{rank}")
-    break
+def sol():
+  rank = 1
+  for c_idx in range(n-1):
+    if country[c_idx][0] == k:
+      return str(rank)
+    if country[c_idx][1:] != country[c_idx+1][1:]:
+      rank = c_idx+2
+  return str(rank)
+
+sys.stdout.write(sol())
+
