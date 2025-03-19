0f45b3b95aa7a7bcfb38b1b8bb6eba3feeac736b
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-03-19 13:46:18 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 68 ms, Memory: 42700 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.py
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.py
@@ -1,13 +1,11 @@
-
 def sliding_window_12847(arr, key):
-  p1, p2, current_sum = 0, key-1, sum(arr[:key])
+  current_sum = sum(arr[:key])
   current_max = current_sum
-  while p2 < len(arr)-1:
-    p2 += 1
-    current_sum += arr[p2]
-    current_sum -= arr[p1]
-    p1 += 1
+
+  for p1 in range(len(arr) - key):
+    current_sum += arr[p1+key] - arr[p1]
     current_max = max(current_max, current_sum)
+
   return current_max
 
 n, key = map(int, input().split())
