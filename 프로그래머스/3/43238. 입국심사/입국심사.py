259134ff16d607d24ca18bb6298419d70d95c42b
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-05-02 15:49:54 +0900

    [level 3] Title: 입국심사, Time: 627.84 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,15 +1,19 @@
 def solution(n, times):
-    left = 1
-    right = max(times) * n
+    left, right = 0, n * max(times)
     answer = right
-    
+
     while left <= right:
-        mid = (left+right) // 2
+        mid = (left + right) // 2
+        print(left, mid, right)
+
+        # total = n 이 되는 범위 (28 ~ 29)
+
         total = sum(mid // t for t in times)
-        
-        if total >= n:
-            answer = mid
-            right = mid-1
+        if total < n:
+            left = mid + 1
+            answer = left
         else:
-            left = mid+1
-    return answer 
+            right = mid - 1
+            
+    print(left, mid, right)
+    return answer
