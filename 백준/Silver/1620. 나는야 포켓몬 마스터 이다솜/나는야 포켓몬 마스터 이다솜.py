9611133a8bcf1e5d5e69d6660269ef7de6a7830b
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-05-06 15:34:54 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 268 ms, Memory: 51528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,20 +1,21 @@
 # HASH TABLE
+import sys
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 pocket_id = dict()
 pocket_name = dict()
 for i in range(1, n+1):
-  name = input()
-  # if name[-1].isupper():
-  #   name = name[0].upper() + name[1:-1] + name[-1].lower()
+  name = sys.stdin.readline().strip()
+  # if (65 <= ord(name[-1]) <= 90):
+  #   name = chr(ord(name[0]) - 32) + name[1:-1] + chr(ord(name[-1]) + 32)
 
   pocket_id[i] = name
   pocket_name[name] = i
 
 for k in range(m):
-  question = input()
+  question = sys.stdin.readline().strip()
 
-  if question.isdigit():
+  if all(48 <= ord(q) <= 57 for q in question):
     print(pocket_id[int(question)])
   else:
     print(pocket_name[question])
