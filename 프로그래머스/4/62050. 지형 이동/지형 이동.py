b51d7dc236b7391da3e333104d3e60a38d8463db
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-05-08 21:21:27 +0900

    [level 4] Title: 지형 이동, Time: 513.77 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.py
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.py
@@ -3,51 +3,28 @@ direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 def valid_c(y, x, length):
   return 0 <= y < length and 0 <= x < length
 
-def find(node, parent):
-  root = node
-  while parent[root] != root:
-    root = parent[root]
-
-  while parent[node] != root:
-    next_node = parent[node]
-    parent[node] = root
-    node = next_node
-
-  return root
-
-def union(node1, node2, parent):
-  root1 = find(node1, parent)
-  root2 = find(node2, parent)
-
-  if root1 == root2:
-    return False
-  parent[root2] = root1
-  return True
-
-def make_edge_data(land):
-  edge = []
-  length = len(land)
-  for y in range(length):
-    for x in range(length):
-      for dy, dx in direction:
-        ny, nx = y+dy, x+dx
-        if valid_c(ny, nx, length):
-          edge.append([ny*length+nx, y*length+x, abs(land[ny][nx] - land[y][x])])
-  edge.sort(key=lambda x: x[2])
-  return edge
-
-# Kruskal
 def solution(land, height):
-  import sys
-  sys.setrecursionlimit(100000)
-  edges = make_edge_data(land)
-  length = len(land)
-  parent = list(range(length * length))
+  import heapq
+
   answer = 0
+  length = len(land)
+  dp = [[False for _ in range(length)] for _ in range(length)]
+  heap = [(0, (0, 0))]
 
-  for node1, node2, weight in edges:
-    if union(node1, node2, parent):
-      if weight > height:
-        answer += weight
+  while heap:
+    weight, coordinate = heapq.heappop(heap)
+    y, x = coordinate
+    if dp[y][x]:
+      continue
+    dp[y][x] = True
+    if weight > height:
+      answer += weight
 
-  return(answer)
+    for dy, dx in direction:
+      ny, nx = y + dy, x + dx
+      if valid_c(ny, nx, length):
+        if dp[ny][nx]:
+          continue
+        next_weight = abs(land[y][x] - land[ny][nx])
+        heapq.heappush(heap, (next_weight, (ny, nx)))
+  return answer
