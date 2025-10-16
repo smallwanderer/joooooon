d34c024546c2f9973643c017b98e2b16d643fbcb
Hyungmin Seo <99022143+smallwanderer@users.noreply.github.com>
2025-10-16 15:28:27 +0900

    [level 3] Title: 봉인된 주문, Time: 1617.63 ms, Memory: 48.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -0,0 +1,31 @@
+# number of alphabets : 26
+# 'aa' -> 1 * (27 ** 1) + 1
+# n보다 작은 크기의 ban을 찾고, n에서 해당 값 빼고, 거기서 26진수 변환
+
+base26_dict = {chr(96+n): n for n in range(1, 27)}
+BASE = 26
+
+def base_to_num(char):
+    length = len(char)
+    result = 0
+    for i in range(length):
+        power = length-i-1
+        result += base26_dict[char[i]] * (BASE ** power)
+    return result
+#print(base_to_num('az'))
+
+def num_to_base(num):
+    result = []
+    while num > 0:
+        char = chr(97+((num-1) % 26))
+        result.append(char)
+        num = (num-1) // 26
+    return ''.join(reversed(result))
+#print(num_to_base(26))
+
+def solution(n, bans):
+    bans = list(map(base_to_num, bans))
+    bans.sort()
+    for b in bans:
+        if b <= n: n += 1
+    return num_to_base(n)
